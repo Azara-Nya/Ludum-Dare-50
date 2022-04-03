0e7b6c4ed7754ef741f2a9e1ab025b18284947a2
Azara-Nya <77995029+Azara-Nya@users.noreply.github.com>
2022-04-04 00:35:59 +0100

    Animations Update
    Player Script Messy Now

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,10 @@ public class Player : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float MoveSpeed;
     [SerializeField] private Menu mn;
+    [SerializeField] private Animator Andy;
     public Sleeper Sz;
+    private float moveInput;
+    private bool facingLeft = true;
     Vector2 movement;
 
     void Update()
@@ -17,7 +20,35 @@ public class Player : MonoBehaviour
             movement.x = Input.GetAxisRaw("Horizontal");
             movement.y = Input.GetAxisRaw("Vertical");
 
+            moveInput = Input.GetAxis("Horizontal");
+
+            if (moveInput == 0)
+            {
+                Andy.SetBool("IsWalk", false);
+            }
+            else
+            {
+                Andy.SetBool("IsWalk", true);
+            }
+
+            if (facingLeft && moveInput > 0)
+            {
+                Fliper();
+            }
+            else if (!facingLeft && moveInput < 0)
+            {
+                Fliper();
+            }
+
             rb.MovePosition(rb.position + movement * MoveSpeed * Time.fixedDeltaTime);
         }
+    }
+
+    void Fliper()
+    {
+        facingLeft = !facingLeft;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 }
